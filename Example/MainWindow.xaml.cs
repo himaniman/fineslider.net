9d6dbb8c56724ccacd1413100848c4619b019df7
himaniman <himaniman@mail.ru>
2023-07-28 12:54:46 +0700

    Some example cleanup

--- Example/MainWindow.xaml.cs
+++ Example/MainWindow.xaml.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Example
 {
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private double valueDouble = 1;
+        private double valueDouble = 1.05;
 
         public double ValueDouble
         {
